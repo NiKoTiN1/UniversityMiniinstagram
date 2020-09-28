b2dcbe19c437c6eb5f52f51189336fd78f7f34d7
NiKoTiN1 <56438316+NiKoTiN1@users.noreply.github.com>
2020-09-28 23:18:09 +0300

    Custom tag-helper added

---
+++ UniversityMiniinstagram.Web/TagHelpers/TimerTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversityMiniinstagram.Web.TagHelpers
+{
+    public class TimerTagHelper: TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Content.SetContent($"Текущее время: {DateTime.Now.ToString("HH:mm:ss")}");
+        }
+    }
+}
