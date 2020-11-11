d0195e44105d8e4acb4150ed12e6057289e2dcef
NikotiN <nikitabiletsky@bk.ru>
2020-11-11 16:06:07 +0300

    a

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 "# UniversityMiniinstagram" 
+This website is available on: https://universityminiinstagram-dev-as.azurewebsites.net/news/all
+
+
+Functional:
+1. Possibility of registration / authorization using Google;
+2. User profile page and the ability to interact with posts.
+3. Comments
+4. Ability to delete a comment or complain, depending on the user's rights.
+5. Roles are given only by the administrator
+6. Moderators can view complaints about posts and comments and decide whether to ban, justify or hide the post (comment).
+7. Complaints about posts (comments) to moderators are considered only by the administrator.
+8. Only the administrator has the rights to unban the user
+9. You can add posts by choosing their category and writing descriptions.
+Do not judge strictly this is one of my first more or less difficult projects (=
--- UniversityMiniinstagram.Web/Hubs/CommentaryHub.cs
+++ UniversityMiniinstagram.Web/Hubs/CommentaryHub.cs
@@ -34,22 +34,25 @@ namespace UniversityMiniinstagram.Web.Hubs
 
         public async Task SendMessage(Guid postId, string text)
         {
-            SendCommentViewModel vm = new SendCommentViewModel()
+            if(text != "" && text != "" && text != " ")
             {
-                PostId = postId,
-                Text = text
-            };
-            var userIdClaim = this.Context.User.Claims.FirstOrDefault(a => a.Type == ClaimTypes.NameIdentifier);
-            var commresult = await _postService.AddComment(vm, userIdClaim.Value);
-            CommentViewModel commentViewModel = new CommentViewModel()
-            {
-                Comment = commresult,
-                IsDeleteRelated = await _postService.isDeleteRelated(commresult.User, userIdClaim.Value),
-                IsReportRelated = await _postService.isReportRelated(commresult.UserId, userIdClaim.Value, commentId: commresult.Id),
-                ShowReportColor = false
-            };
-            var res = await _renderService.RenderToStringAsync("_CommentBlock", commentViewModel);
-            await Clients.All.SendAsync("SendCommentHub", res, postId);
+                SendCommentViewModel vm = new SendCommentViewModel()
+                {
+                    PostId = postId,
+                    Text = text
+                };
+                var userIdClaim = this.Context.User.Claims.FirstOrDefault(a => a.Type == ClaimTypes.NameIdentifier);
+                var commresult = await _postService.AddComment(vm, userIdClaim.Value);
+                CommentViewModel commentViewModel = new CommentViewModel()
+                {
+                    Comment = commresult,
+                    IsDeleteRelated = await _postService.isDeleteRelated(commresult.User, userIdClaim.Value),
+                    IsReportRelated = await _postService.isReportRelated(commresult.UserId, userIdClaim.Value, commentId: commresult.Id),
+                    ShowReportColor = false
+                };
+                var res = await _renderService.RenderToStringAsync("_CommentBlock", commentViewModel);
+                await Clients.All.SendAsync("SendCommentHub", res, postId);
+            }
         }      
     }
 }
